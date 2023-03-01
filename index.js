dfeec6551750638d52f483db730cffa8151cf704
a60231023 <ayushtiwari6061@gmail.com>
2023-03-01 23:16:09 +0530

    basic done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const parse = require('csv-parse');
+const parser = require('csv-parse'); 
+const fs = require('fs');
+
+const results = [];
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+}
+fs.createReadStream('./kepler-data.csv')
+    .pipe(parser({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if(isHabitablePlanet(data))
+            results.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });
